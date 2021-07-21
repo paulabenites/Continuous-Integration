5c5fae9e37697c68536c4cfe432ebf5224508749
Paula Benites <paula.beni.tes@hotmail.com>
2021-07-21 16:19:06 -0500

    Test cases

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -19,14 +19,26 @@ public class RelationalOperatorTest {
 	}
 
 	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(2, 2));
+	}
+	
+	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertTrue(tester.isLess(1, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+	
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isLess(5, 5));
 	}
 }
